28b71c557076649e7bfa38a466ab86b515aafa4b
Joey <jdechant970@gmail.com>
2018-04-26 09:56:33 -0600

    4.26

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ function randomWord(array){
 
 
 
+
+
 var activeGame = false;
 var wordBank = [
     "Hodor",
@@ -34,10 +36,17 @@ var wordBank = [
 
 var winCount = 0;
 var lossCount = 0;
+
 var gameActive = false;
-var lives = 10;
+var userLost = false;
+var userWon = false;
+
+var lives = 5;
 var guessWord;
+var hiddenWord = "_";
+var remainingWord;
 var guessed = [];
+var charRemaining = 0;
 
 var correct = [];
 var incorrect = [];
@@ -47,12 +56,27 @@ var misses = 0;
 
 
 document.onkeypress = function(event){
-    if (gameActive === false){    
+    if (gameActive === false){     //DETERMINES IF THE GAME HAS BEEN STARTED, INITIATES NEW GAME IF FALSE
         guessWord = randomWord(wordBank).toUpperCase();
+        remainingWord = guessWord;
+        hiddenWord = "_";
+        guessed = [];
+        correct = [];
+        incorrect = [];
+        lives = 5;
+
         var charCount = guessWord.length;
+        for(var i = 1; i < charCount; i++){
+            hiddenWord = hiddenWord + "_";
+            console.log(hiddenWord);
+        }
+
         console.log(guessWord);
         gameActive = true;
-    } else {
+        charRemaining = charCount;
+        userLost = false;
+        userWon = false;
+    } else {   
         var guess = event.key.toUpperCase();
         var charGuessed = false;
         for(var j = 0; j < guessed.length; j++){
@@ -69,12 +93,17 @@ document.onkeypress = function(event){
             for(var i = 0; i < guessWord.length; i++){
                 if(guess == guessWord.charAt(i)){
                     correct.push(guess);
-                    guessWord = guessWord.slice(0,i) + guessWord.slice(i+1);
+                    hiddenWord = hiddenWord.slice(0,i) + guessWord.charAt(i) + hiddenWord.slice(i+1);
+                    remainingWord = remainingWord.slice(0,remainingWord.indexOf(guess)) + remainingWord.slice(remainingWord.indexOf(guess)+1);
+
                     charCorrect = true;
 
                 }  
                 
-                console.log(guessWord);
+                charRemaining = remainingWord.length;
+                console.log("guess Word: " + guessWord);
+                console.log("hidden word " +hiddenWord);
+                console.log("remaining word " +remainingWord);
                 
             }
             console.log("charCorrect " + charCorrect);
@@ -82,12 +111,32 @@ document.onkeypress = function(event){
             if(charCorrect === false){
                 incorrect.push(guess);
                 misses = incorrect.length;
+                lives = lives - 1
+            }
+
+            if(lives === 0){
+                gameActive = false;
+                userLost = true;
+                charRemaining = 0;
+                lossCount = lossCount+1;
+                console.log("losses: " + lossCount);
+                console.log("wins :" + winCount);
+
+            }else if(charRemaining === 0){
+                gameActive = false;
+                charRemaining = 0;
+                userWon = true;
+                winCount = winCount+1;
+                console.log("losses: " + lossCount);
+                console.log("wins :" + winCount);
+
             }
 
             console.log("incorrect:" + incorrect);
             console.log("correct: " +correct);
-            console.log("guessed: " + guessWord[i]);
-            console.log(lives - misses);
+            // console.log("guessed: " + guessWord[i]);
+            console.log(lives);
+            console.log("letters remaining " + charRemaining);
 
 
 
