71567565a7544d0a266488f79c106cbc8a1b82c5
Joey <jdechant970@gmail.com>
2018-06-17 11:58:02 -0600

    filtered our non-alpha-numeric keypresses, build README

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,4 @@
-function randomWord(array){
-   return array[Math.floor(Math.random()*array.length)];
-}
-
-function randomIndex(array){
-    return Math.floor(Math.random()*array.length);
-}
-
-
-
-
-
+//GLOBAL VARIABLES
 var activeGame = false;
 var alphaNumeric = ["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var wordBank = [
@@ -37,7 +26,7 @@ var wordBank = [
     "Tommen",
     "Daeneryis",
     "Theon"
-]
+];
 
 var winCount = 0;
 var lossCount = 0;
@@ -65,25 +54,38 @@ var rightMessage = [
 var wrongMessage = [
 ]
 
+
+// Array of messages that are randomly selected when game is lost
 var loseMessage = [
     "You know nothing Jon Snow",
     "The Lannisters send their regards",
     "Everyone is mine to torment.",
     "The night is dark and full of terrors.",
     "If you think this has a happy ending, you haven't been paying attention.",
+];
 
-
-]
+//Array of messages that are randomly selected when the game is won
 var winMessage = [
     "That's what I do, I drink and I know things",
     "What do we say to the God of death?",
     "Chaos isn't a pit. Chaos is a ladder."
     ]
 
+//grabs value of random index inside an array
+function randomWord(array){
+    return array[Math.floor(Math.random()*array.length)];
+}
+ 
+//grabs random index inside an array
+function randomIndex(array){
+    return Math.floor(Math.random()*array.length);
+}
 
-
+//controls the everything that happens on a keypress
 document.onkeypress = function(event){
-    if (gameActive === false){     //DETERMINES IF THE GAME HAS BEEN STARTED, INITIATES NEW GAME IF FALSE
+//first part of if statement : starts new game if gameActive = false
+//handles reseting of global variables and printing wins, losses, message and main image to page
+    if (gameActive === false){ 
         guessWord = randomWord(wordBank).toUpperCase();
         remainingWord = guessWord;
         hiddenWord = "_";
@@ -99,43 +101,47 @@ document.onkeypress = function(event){
         $(".lives, .hidden-word, .message").css("color" , "rgba(255," + lives*55 + "," + lives*55 + ",1" );
         $(".guessed-letters").empty();
         document.querySelector(".img-wrap img").setAttribute("src" , "assets/images/" + lives + "_lives.jpg")
-
         var charCount = guessWord.length;
+
+//builds a string of underscores that matches length of the guessWord
         for(var i = 1; i < charCount; i++){
             hiddenWord = hiddenWord + "_";
-            // console.log(hiddenWord);
             document.querySelector(".hidden-word").innerHTML = hiddenWord;
         }
 
-        // console.log(guessWord);
         gameActive = true;
         charRemaining = charCount;
         userLost = false;
         userWon = false;
-        
+     
+//second part of if statement runs if game is already in progress
     } else {   
+//sets the guess equal to key pressed
         var guess = event.key.toUpperCase();
         var charGuessed = false;
+        var charValid = false;
 
-        // for(var k = 0; k < alphaNumeric.length; k++){
-        //     console.log(alphaNumeric[k]);
-        //     if(alphaNumeric[k] !== guess){
-        //         charGuessed = true;
-        //     }
-        // }
-
+//runs through bank of guessed letters to determine if letter has already been guessed
         for(var j = 0; j < guessed.length; j++){
             if(guessed[j] === guess){
                 charGuessed = true;
             }
         }
-        
 
-        if(charGuessed){
-            // console.log("guess again, dumbass");
-        
+//checks if the key pressed is alpha-numeric
+        for(var j = 0; j < alphaNumeric.length; j++){
+            if(alphaNumeric[j] === guess){
+                charValid = true;
+            }
+        } 
+//checks if guess has been guessed and is a valid character
+        if(charGuessed || charValid === false){
+   
         } else{
             var charCorrect = false;
+//checks where the letter guessed matches up with letters inside guessWord, replaces them in hiddenWord
+//pushes letter to guessed array
+//identifies that guess was correct
             for(var i = 0; i < guessWord.length; i++){
                 if(guess == guessWord.charAt(i)){
                     correct.push(guess);
@@ -147,18 +153,18 @@ document.onkeypress = function(event){
                 }  
                 
                 charRemaining = remainingWord.length;
-                // console.log("guess Word: " + guessWord);
-                // console.log("hidden word " +hiddenWord);
+//prints updated word to page
                 document.querySelector(".hidden-word").innerHTML = hiddenWord;
-                // console.log("remaining word " +remainingWord);
                 
             }
-            // console.log("charCorrect " + charCorrect);
-            
+
+//if guess is wrong : pushes letter to guessed array
+//deducts a life
+//reduces non-red color values by 1/total lives * 255
             if(charCorrect === false){
                 incorrect.push(guess);
                 misses = incorrect.length;
-                lives = lives - 1
+                lives--;
                 document.querySelector(".img-wrap img").setAttribute("src" , "assets/images/" + lives + "_lives.jpg")
                 $(".lives").text(lives);
                 $(".lives, .hidden-word, .message").css("color" , "rgba(255," + lives*55 + "," + lives*55 + ",1" );
@@ -166,25 +172,25 @@ document.onkeypress = function(event){
 
             $(".guessed-letters").append(guess);
 
+//determines if all lives are used up
+//if true : ends game, tallies a loss
             if(lives === 0){
                 gameActive = false;
                 userLost = true;
                 charRemaining = 0;
                 lossCount = lossCount+1;
-                // console.log("losses: " + lossCount);
-                // console.log("wins :" + winCount);
                 document.querySelector(".losses span").innerHTML = lossCount;
                 document.querySelector(".hidden-word").innerHTML = guessWord;
                 document.querySelector(".message").innerHTML = loseMessage[randomIndex(loseMessage)];
                 $(".start").css("opacity", "1");
 
+//determines if all etters have been guessed
+//if true : ends game, tallies a win
             }else if(charRemaining === 0){
                 gameActive = false;
                 charRemaining = 0;
                 userWon = true;
                 winCount = winCount+1;
-                // console.log("losses: " + lossCount);
-                // console.log("wins :" + winCount);
                 document.querySelector(".wins span").innerHTML = winCount;
                 document.querySelector(".hidden-word").innerHTML = guessWord;
                 document.querySelector(".message").innerHTML = winMessage[randomIndex(winMessage)];
@@ -193,16 +199,7 @@ document.onkeypress = function(event){
 
             }
 
-            // console.log("incorrect:" + incorrect);
-            // console.log("correct: " +correct);
-            // console.log("guessed: " + guessWord[i]);
-            // console.log(lives);
-            // console.log("letters remaining " + charRemaining);
-
-
-
             guessed.push(guess);
-            // console.log("letters guessed: " + guessed);
         }
     }
 
